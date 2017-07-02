5bc0fa5292f6327134d2ec1dc325b71fed388044
peps0791 <piyush.papreja07@gmail.com>
2017-07-02 16:03:00 -0700

    UI updated

---
+++ app/src/main/java/org/mobilecomputing/projects/brainnet/LoginActivity.java
@@ -0,0 +1,33 @@
+package org.mobilecomputing.projects.brainnet;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button enterBtn;
+    Intent enterIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        enterBtn = (Button)findViewById(R.id.enter);
+
+         /*button listeners*/
+        /*listener for enter button*/
+        enterBtn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                enterIntent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(enterIntent);
+
+            }
+        });
+    }
+}
--- app/src/main/java/org/mobilecomputing/projects/brainnet/MainActivity.java
+++ app/src/main/java/org/mobilecomputing/projects/brainnet/MainActivity.java
@@ -48,20 +48,10 @@ import static android.R.id.message;
 
 public class MainActivity extends AppCompatActivity{
 
-
-    public static final int MULTIPLE_PERMISSIONS = 10; // code you want.
-    ToggleButton bluetoothToggle;
-    BluetoothAdapter mBluetoothAdapter;
-    BluetoothDevice mmDevice;
-    Button connect;
-    private Socket client;
-    private PrintWriter printwriter;
-    private EditText textField;
-    private Button button;
-    private String messsage;
-    int stopThread = 0;
-
-
+    Button loginBtn;
+    Button registerBtn;
+    Intent registerIntent;
+    Intent loginIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,250 +59,34 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
 
-        if (checkPermissions()){
-            // permissions granted.
+        loginBtn = (Button)findViewById(R.id.login);
+        registerBtn = (Button)findViewById(R.id.register);
 
 
-
-        } else {
-            // show dialog informing them that we lack certain permissions
-        }
-
-
-        this.bluetoothToggle = (ToggleButton)findViewById(R.id.bltSwitch) ;
-        this.bluetoothToggle.setOnCheckedChangeListener( new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton toggleButton, boolean isChecked) {
-                toggleBluetooth(isChecked);
-            }
-        }) ;
-
-        // Register for broadcasts when a device is discovered.
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        filter.addAction(BluetoothDevice.ACTION_FOUND);
-        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-        registerReceiver(mReceiver, filter);
-
-
-
-        connect = (Button)findViewById(R.id.connect);
-
-        connect.setOnClickListener(new View.OnClickListener()
+        /*button listeners*/
+        /*listener for login button*/
+        loginBtn.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
             {
-                new ConnectToServer().execute();
+                loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+
+            }
+        });
+
+        /*listener for register button*/
+        registerBtn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+
             }
         });
 
     }
-
-    // Create a BroadcastReceiver for ACTION_FOUND.
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            Log.d(Constants.CUSTOM_LOG_TYPE, "action :" +action);
-
-            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
-                //discovery starts, we can show progress dialog or perform other tasks
-                Log.d(Constants.CUSTOM_LOG_TYPE, "Device discovery started");
-
-            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-                //discovery finishes, dismis progress dialog
-                Log.d(Constants.CUSTOM_LOG_TYPE, "Finished scanning devices");
-
-            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                //bluetooth device found
-                BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                Log.d(Constants.CUSTOM_LOG_TYPE, "Found device " + device.getName());
-            }
-        }
-
-    };
-
-
-    private class ConnectToServer extends AsyncTask<String, Void, Void> {
-
-        private Exception exception;
-
-        protected Void doInBackground(String... urls) {
-            /** Define a host server */
-            String host = Constants.HOST;
-            /** Define a port */
-            int port = Constants.PORT;
-
-            StringBuffer instr = new StringBuffer();
-            String TimeStamp;
-            Log.d(Constants.CUSTOM_LOG_TYPE, "SocketClient initialized");
-
-            try {
-                /** Obtain an address object of the server */
-                InetAddress address = InetAddress.getByName(host);
-                /** Establish a socket connetion */
-                Socket connection = new Socket(address, port);
-                /** Instantiate a BufferedOutputStream object */
-
-                BufferedOutputStream bos = new BufferedOutputStream(connection.
-                        getOutputStream());
-
-                /** Instantiate an OutputStreamWriter object with the optional character
-                 * encoding.
-                 */
-                OutputStreamWriter osw = new OutputStreamWriter(bos, "US-ASCII");
-
-                TimeStamp = new java.util.Date().toString();
-                String process = "Calling the Socket Server on "+ host + " port " + port +
-                        " at " + TimeStamp +  (char) 13;
-
-                /** Write across the socket connection and flush the buffer */
-                osw.write(process);
-                osw.flush();
-
-                /** Instantiate a BufferedInputStream object for reading
-                 /** Instantiate a BufferedInputStream object for reading
-                 * incoming socket streams.
-                 */
-
-                BufferedInputStream bis = new BufferedInputStream(connection.
-                        getInputStream());
-                /**Instantiate an InputStreamReader with the optional
-                 * character encoding.
-                 */
-
-                InputStreamReader isr = new InputStreamReader(bis, "US-ASCII");
-
-                /**Read the socket's InputStream and append to a StringBuffer */
-                int c;
-                while ( (c = isr.read()) != 13)
-                    instr.append( (char) c);
-
-                /** Close the socket connection. */
-                connection.close();
-                Log.d(Constants.CUSTOM_LOG_TYPE, "server message-->" +instr);
-            }
-            catch (IOException f) {
-                System.out.println("IOException: " + f);
-            }
-            catch (Exception g) {
-                System.out.println("Exception: " + g);
-            }
-
-            return null;
-        }
-    }
-
-
-    private void toggleBluetooth(boolean isChecked){
-        //toggle bluetooth
-        Log.d(Constants.CUSTOM_LOG_TYPE, "toggle button ->" +isChecked);
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (!isChecked && mBluetoothAdapter.isEnabled()) {
-            boolean isDisabled = mBluetoothAdapter.disable();
-            Log.d(Constants.CUSTOM_LOG_TYPE, "disabled bluetooth ?" +isDisabled);
-        }else if(isChecked && !mBluetoothAdapter.isEnabled()){
-            boolean isEnabled = mBluetoothAdapter.enable();
-            Log.d(Constants.CUSTOM_LOG_TYPE, "enabled bluetooth ?" +isEnabled);
-            discoverDevices();
-            //findBT();
-        }
-    }
-
-
-    public void discoverDevices(){
-
-        mBluetoothAdapter =  BluetoothAdapter.getDefaultAdapter();
-        if(mBluetoothAdapter.isDiscovering()){
-            mBluetoothAdapter.cancelDiscovery();
-        }
-        boolean result = mBluetoothAdapter.startDiscovery();
-        Log.d(Constants.CUSTOM_LOG_TYPE, "Device discovery started ? " + result);
-
-    }
-
-    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
-        new AlertDialog.Builder(MainActivity.this)
-                .setMessage(message)
-                .setPositiveButton("OK", okListener)
-                .setNegativeButton("Cancel", null)
-                .create()
-                .show();
-    }
-
-
-    private boolean checkPermissions() {
-        int result;
-        List<String> listPermissionsNeeded = new ArrayList<>();
-        for (String p:Constants.permissions) {
-            result = ContextCompat.checkSelfPermission(MainActivity.this,p);
-            if (result != PackageManager.PERMISSION_GRANTED) {
-                listPermissionsNeeded.add(p);
-            }
-        }
-        if (!listPermissionsNeeded.isEmpty()) {
-            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), MULTIPLE_PERMISSIONS);
-            return false;
-        }
-        return true;
-    }
-
-
-    void findBT()
-    {
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if(mBluetoothAdapter == null)
-        {
-            Log.d(Constants.CUSTOM_LOG_TYPE, "No bluetooth adapter available");
-        }
-
-        if(!mBluetoothAdapter.isEnabled())
-        {
-            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBluetooth, 0);
-        }
-
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-
-
-        if(pairedDevices.size() > 0)
-        {
-            for(BluetoothDevice device : pairedDevices)
-            {
-                if(device.getName().equals("Samsung"))
-                {
-                    Log.d(Constants.CUSTOM_LOG_TYPE, "device address->" +device.getAddress());
-                    mmDevice = device;
-                    break;
-                }
-            }
-        }
-        Log.d(Constants.CUSTOM_LOG_TYPE, "Bluetooth Device Found");
-    }
-
-    void sendData() throws IOException
-    {
-        /*String msg = myTextbox.getText().toString();
-        msg += "\n";
-        mmOutputStream.write(msg.getBytes());
-        myLabel.setText("Data Sent");*/
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MULTIPLE_PERMISSIONS:{
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    // permissions granted.
-                    Log.d(Constants.CUSTOM_LOG_TYPE, "Permission granted!!");
-
-                } else {
-                    // no permissions granted.
-                }
-                return;
-            }
-        }
-    }
-
 
     @Override
     protected void onResume() {
@@ -322,9 +96,5 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-
-        // Don't forget to unregister the ACTION_FOUND receiver.
-        unregisterReceiver(mReceiver);
     }
 }
