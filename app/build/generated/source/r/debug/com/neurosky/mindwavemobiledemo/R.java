f47cea7b7c46b2e7caa30d949132c3202eeb9409
mishrakrishnakant <kmishra3@asu.edu>
2017-07-02 23:39:24 -0700

    Second commit

--- app/build/generated/source/r/debug/com/neurosky/mindwavemobiledemo/R.java
+++ app/build/generated/source/r/debug/com/neurosky/mindwavemobiledemo/R.java
@@ -54,8 +54,9 @@ public final class R {
     public static final class drawable {
         public static final int gouxuan1=0x7f020000;
         public static final int gouxuan2=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int select_device_bg=0x7f020003;
+        public static final int ic_info=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int select_device_bg=0x7f020004;
     }
     public static final class id {
         public static final int AbsoluteLayout1=0x7f090000;
